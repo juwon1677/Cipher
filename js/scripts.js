349a429cd52e2e928ffab3b0cab1a3e63bd3d94d
adeniran adejuwon <adeniran adejuwon>
2023-11-23 01:44:21 -0800

    add function 1

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+let sentence = prompt("Type your sentence here")
+var text = sentence;
+
+
+// to get the first and last letter of my sentence and turn to a string
+
+function newString(){
+  let value = text.charAt(0) + text.charAt(text.length - 1);
+  value.toUpperCase();
+  return value.toString
+}
+
+
+// to get the first and last letter of my sentence and turn to a string , then reverse it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
